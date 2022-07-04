4fb4089a63d41d92df21dd245d097e2b777224e0
AlejandroSparza77 <stevealexjessepersson@gmail.com>
2022-07-04 06:36:44 +0000

    Validate that a username does not exist when registering

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+package main.java;
+
+/** An item on a todo list. */
+public final class Task {
+
+    private final long id;
+    private final String names;
+    private final String lastNames;
+    private final String username;
+    private final long timestamp;
+
+    public Task(long id, String names, String lastNames, String username,
+    long timestamp) {
+    
+        this.id = id;
+        this.names = names;
+        this.lastNames = lastNames;
+        this.username = username;
+        this.timestamp = timestamp;
+
+    }
+
+    public String getUsername() {
+
+        return this.username;
+
+    }
+    
+}
--- src/main/java/com/google/sps/servlets/RegisterHandlerServlet.java
+++ src/main/java/com/google/sps/servlets/RegisterHandlerServlet.java
@@ -10,6 +10,13 @@ import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
+import java.util.ArrayList;
+import java.util.List;
+import main.java.Task;
+import com.google.gson.Gson;
 
 @WebServlet("/register-handler")
 public class RegisterHandlerServlet extends HttpServlet {
@@ -23,6 +30,7 @@ public class RegisterHandlerServlet extends HttpServlet {
     String username = request.getParameter("username");
     String password = request.getParameter("password");
     long timestamp = System.currentTimeMillis();
+    boolean newUsername = true;
 
     // Print the value so you can see it in the server logs:
     System.out.println("Your name(s): " + names);
@@ -33,22 +41,91 @@ public class RegisterHandlerServlet extends HttpServlet {
 
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
-    FullEntity taskEntity =
-        Entity.newBuilder(keyFactory.newKey())
-            .set("Name(s)", names)
-            .set("Last name(s)", lastNames)
-            .set("Username", username)
-            .set("Password", password)
-            .set("Timestamp", timestamp)
-            .build();
-    datastore.put(taskEntity);
 
+    Query<Entity> query =
+        Query.newEntityQueryBuilder().setKind("Task").setOrderBy(OrderBy.desc("timestamp")).build();
+    QueryResults<Entity> results = datastore.run(query);
+
+    List<Task> tasks = new ArrayList<>();
+    while (results.hasNext()) {
+
+        Entity entity = results.next();
+
+        long id = entity.getKey().getId();
+        String previousNames = entity.getString("names");
+        String previousLastNames = entity.getString("lastNames");
+        String previousUsername = entity.getString("username");
+        long previousTimestamp = entity.getLong("timestamp");
+
+        Task task = new Task(id, previousNames, previousLastNames,
+            previousUsername, previousTimestamp);
+        tasks.add(task);
+
+    }
+
+    for (int i = 0; i < tasks.size(); i++) {
+
+        if (tasks.get(i).getUsername().equals(username)) {
+            newUsername = false;
+            break;
+        }
+
+    }
+    
+    /*
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(tasks));
+    */
+    
+    /*
+    for (int j = 0; j < usernames.length; j++) {
+
+        if (usernames[j] == username) {
+
+            newUsername = false;
+            break;
+
+        }
+
+    }
+*/
+
+    if (newUsername) {
+
+        FullEntity taskEntity =
+            Entity.newBuilder(keyFactory.newKey())
+                .set("names", names)
+                .set("lastNames", lastNames)
+                .set("username", username)
+                .set("password", password)
+                .set("timestamp", timestamp)
+                .build();
+        datastore.put(taskEntity);
+
+        response.sendRedirect("rightRegister.html");
+
+    }
+    else {
+
+        response.sendRedirect("wrongRegister.html");
+
+    }
+
+    /*
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(tasks));
+    */
+
+    /*
     // Write the value to the response so the user can see it.
     response.getWriter().println("Your name(s): " + names
         + "\nYour last name(s): " + lastNames
         + "\nYour username: " + username
         + "\nYour password: " + password
         + "\nYour timestamp: " + timestamp);
+    */
 
   }
 
